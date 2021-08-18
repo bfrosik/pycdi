0584476a39e361ea8c60113665883098d2a722a9
Barbara Frosik <bfrosik@anl.gov>
2021-08-18 16:54:24 -0500

    Update rec.py

--- pycohere/controller/rec.py
+++ pycohere/controller/rec.py
@@ -223,6 +223,7 @@ class Rec:
                           self.shrink_wrap_trigger,
                           self.phase_support_trigger,
                           self.to_reciprocal_space,
+                          self.new_func,
                           self.pcdi_trigger,
                           self.pcdi,
                           self.no_pcdi,
@@ -414,6 +415,10 @@ class Rec:
         self.rs_amplitudes = devlib.ifft(self.ds_image) * devlib.size(self.data)
 
 
+    def new_func(self):
+        print('in new_func, new_param', self.params.new_param)
+        
+        
     def pcdi_trigger(self):
         self.pcdi_obj.update_partial_coherence(devlib.absolute(self.rs_amplitudes).copy())
 
